6307b180d57f36506a232d647dbcf08bea0141bd
Jonas Wirth <jonas.wirth@learnet.se>
2019-09-18 13:37:55 +0200

    skrivit klart programmet om vad som händer på väg till skolan

--- Program.py
+++ Program.py
@@ -14,7 +14,7 @@ duscha = input("flyttar du på brödrosten? [y/n] ").lower() # Berättar för pr
 
 if duscha == "n": # Om svaret på rad 13 är "n" får vi en elstöt enligt rad 16 och programmet stängs av
     print("du får en elstöt i duschen och ligger och krampar på golvet de närmaste dagarna tills någon hittar dig")
-    exit() # stänger av programmet
+    exit("Game over LOSER!") # stänger av programmet
 elif duscha == "y": # Om svaret på rad 13 är y går du vidare i programmet till rad 25 och får meddelandet från rad 19
     print("Du blir äntligen ren efter gårdagens kaos.")
 else:
@@ -31,4 +31,23 @@ while season == False: # Så länge "season == false" kommer loopen backa tillba
     elif season == "sommar": # Om vi skriver sommar på rad 27 kommer du hit och rad 32 printas ut i konsollen
         print("Det är skitvarmt ute, överväger att gå naken")
     else: # Om du skriver något annat än en årstid eller stavar fel, kommer du tillbaka till rad 26 eftersom rad 34 ger värdet "season = false" igen
+        season = False
+
+# Ta sig till skolan
+resa = False
+while resa == False:
+    resa = input("hur tar du dig till skolan? [hoverboard, rollerscates, rymdraket, teleportör]").lower()
+    if resa == "hoverboard":
+        print("Du blir mobbad för att du har för lite clout, blir nedslagen och dödad")
+    
+    elif resa == "rollerscates":
+        print("Du får punktering på ett däck och hamnar på trottoarkanten, där ser du en clown i kloakerna med en röd ballong, du syns aldrig igen")
+    
+    elif resa == "rymdraket":
+        print("Trodde du på riktigt att du skulle ta dig till skolan i en raket? Du är dummare än du ser ut.")
+    
+    elif resa == "teleportör":
+        print("Du vet inte vart du hamnar, men du står framför 2 aliens som spelar in porr, du joinar och tjänar en förmögenhet på en annan planet")
+    
+    else:
         season = False
